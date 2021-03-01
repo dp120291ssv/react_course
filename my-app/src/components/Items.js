63163c1cc6e42eefd3412a8bf908233d0b2c7301
Sergei Semenov <dp120291ssv@gmail.com>
2021-03-01 16:40:37 +0200

    fix3

--- my-app/src/components/Items.js
+++ my-app/src/components/Items.js
@@ -5,9 +5,11 @@ export default class Items extends Component {
     return (
       <div id="container">
         <ul>
-          {this.props.todos.map((el) => {
-            return <li key={el.id} className={(el.completed) ? 'completed' : 'active'}>{el.title}</li>;
-          })}
+          {this.props.todos.map((el) => (
+            <li key={el.id} className={el.completed ? "completed" : "active"}>
+              {el.title}
+            </li>
+          ))}
         </ul>
       </div>
     );

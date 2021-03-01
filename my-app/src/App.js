dd26f6218bbbef4ce293fd2d852e332cb92b758d
Sergei Semenov <dp120291ssv@gmail.com>
2021-03-01 14:37:16 +0200

    hw 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,43 @@
-import React, { Component } from 'react'
-import Header from './components/Header';
-import Container from './components/Container';
-import Footer from './components/Footer';
-import './App.css'
+import React, { Component } from "react";
+import Item from "./components/Item";
+import "./App.css";
 
 export default class App extends Component {
+  state = {
+    todos: [
+      {
+        id: "14",
+        title: "Costa Rican Colon North Carolina parsing",
+        completed: true,
+      },
+      {
+        id: "16",
+        title: "Industrial indexing context-sensitive",
+        completed: false,
+      },
+      {
+        id: "18",
+        title: "North Korean Won",
+        completed: false,
+      },
+      {
+        id: "19",
+        title: "Wooden Indiana",
+        completed: true,
+      },
+      {
+        id: "20",
+        title: "Assistant actuating implement",
+        completed: false,
+      },
+    ],
+  };
+
   render() {
     return (
-      <div id='app'>
-        <Header/>
-        <Container/>
-        <Footer/>
+      <div id="app">
+        <Item todos={this.state.todos}/>
       </div>
-    )
+    );
   }
 }
---
+++ my-app/src/components/Item.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Item extends Component {
+  render() {
+    return (
+      <div id="container">
+        <ul>
+          {this.props.todos.map((el) => {
+            return <li key={el.id} className={(el.completed) ? 'completed' : 'active'}>{el.title}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}

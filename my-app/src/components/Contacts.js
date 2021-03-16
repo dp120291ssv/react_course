f5c5e2475f78605e28ad142280fdde2700a6ca2e
Sergei Semenov <dp120291ssv@gmail.com>
2021-03-16 14:09:08 +0200

    add changes

--- my-app/src/components/Contacts.js
+++ my-app/src/components/Contacts.js
@@ -1,73 +1,60 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import {
   createContact,
   deleteContact,
   getContacts,
-  updateContact
 } from "../services/contactsService";
 import ContactsList from "./ContactsList";
 import ContactsForm from "./ContactsForm";
 
-export default class Contacts extends Component {
-  state = {
-    list: [],
-    showContactForm: false,
-  };
+const initialState = {
+  list: [],
+  showContactForm: false,
+};
 
-  componentDidMount() {
-    getContacts().then((list) => this.setState({ list }));
-  }
+export default function Contacts() {
+  const [state, setState] = useState(initialState);
 
-  onAddContactBtn = () => {
-    this.setState({
-      showContactForm: !this.state.showContactForm,
+  useEffect(() => {
+    getContacts().then((list) => setState({ ...initialState, list }));
+  });
+
+  const onAddContactBtn = () => {
+    setState((prevState) => {
+      return { ...prevState, showContactForm: true };
     });
   };
 
-  createContact = (newItem) => {
+  const create = (newItem) => {
     createContact(newItem).then((data) => {
-      this.setState({ list: [...this.state.list, data] });
+      setState({ ...state, list: [...state.list, data] });
     });
   };
 
-  // updateContact = (newItem) => {
-  //   const newItem = this.state.list.find((el) => el.id === id);
-
-  //   updateContact(newItem).then(() => {
-  //     this.setState({
-  //       list: this.state.list.map((newItem) => (newItem.id !== id ? newItem : newItem)),
-  //     });
-  //   });
-  // };
-
-  deleteItem = (id) => {
+  const deleteItem = (id) => {
     deleteContact(id);
-    this.setState({
-      list: this.state.list.filter((item) => item.id !== id),
-    });
+    setState({ ...state, list: state.list.filter((item) => item.id !== id) });
   };
 
-  render() {
-    return (
-      <>
-        <div className="main-content-wrapper">
-          <div className="left-form-wrapper">
-            <ContactsList
-              list={this.state.list}
-              onDelete={this.deleteItem}
-              onEdit={this.updateContact}
-            />
-            <button className="add-btn" onClick={this.onAddContactBtn}>
-              {!this.state.showContactForm ? "Add Contact" : "Hide Form"}
-            </button>
-          </div>
-          <div className="right-form-wrapper">
-            {this.state.showContactForm ? (
-              <ContactsForm onSave={this.createContact} />
-            ) : null}
-          </div>
+  return (
+    <>
+      <div className="main-content-wrapper">
+        <div className="left-form-wrapper">
+          <ContactsList
+            list={state.list}
+            onDelete={deleteItem}
+            // onEdit={updateContact}
+          />
+          <button className="add-btn" onClick={onAddContactBtn}>
+            {!state.showContactForm ? "Add Contact" : "Hide Form"}
+          </button>
         </div>
-      </>
-    );
-  }
+        <div className="right-form-wrapper">
+          {state.showContactForm ? (
+            <ContactsForm onSave={create} />
+          ) : null}
+        </div>
+      </div>
+    </>
+  );
 }

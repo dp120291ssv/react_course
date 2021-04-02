589200c7b0ee5a8449cc0b56be47fd317835c2c6
Sergei Semenov <dp120291ssv@gmail.com>
2021-04-02 17:31:02 +0300

    add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,17 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React, { Component, useCallback, useState } from "react";
-import Users from "./components/menus/Users"
-import Alboms from "./components/menus/Alboms"
+import Users from "./components/menus/Users";
+import Alboms from "./components/menus/Alboms";
 import Navbar from "./components/Navbar";
-import api from "./service/api"
+import api from "./service/api";
 import "./App.css";
 
 export default function App() {
   const [someData, setSomeData] = useState([]);
 
-  const getData = useCallback(
-    (path) => {
-        api.get(path).then(({ data }) =>
-        setSomeData(data)
-    );
-    },
-    [],
-)
-
-getData()
+  const getData = useCallback((path) => {
+    api.get(path).then(({ data }) => setSomeData(data));
+  }, []);
 
   return (
     <Router>

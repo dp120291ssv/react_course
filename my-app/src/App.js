66d3454e0155de296a0f8d27ae70d2e4238f823f
Sergei Semenov <dp120291ssv@gmail.com>
2021-04-02 11:48:00 +0300

    react router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react'
-import Header from './components/Header';
-import Container from './components/Container';
-import Footer from './components/Footer';
-import './App.css'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+import "./App.css";
 
 export default class App extends Component {
   render() {
     return (
-      <div id='app'>
-        <Header/>
-        <Container/>
-        <Footer/>
+      <div id="app">
+        <Router>
+          <Switch>
+            <Route path="/">
+              <Header />
+              <Container />
+              <Footer />
+            </Route>
+          </Switch>
+        </Router>
       </div>
-    )
+    );
   }
 }

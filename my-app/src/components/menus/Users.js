ef0eaa6d3c9cb55bc88046e522edf10f2ab9b854
Sergei Semenov <dp120291ssv@gmail.com>
2021-04-03 21:12:44 +0300

    aa

--- my-app/src/components/menus/Users.js
+++ my-app/src/components/menus/Users.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../../service/api";
 
 export default function Users() {
+
+  const initialState = {
+    list: [],
+  };
+
+  const [someData, setSomeData] = useState(initialState);
+
+  useEffect(() => {
+    api.get('').then(({ list }) => setSomeData({ ...initialState, list }));
+  }, []);
+
+  // const name = list.map((user) => (user.id))
+  
+
   return (
-    <div>Users 123</div>
+    <div>Users 123</div>,
+    <div>{someData.list}</div>
   );
 }
